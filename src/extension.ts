a774b36d4316abcf0a7c4190ae16eb8f07e09629
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2020-10-06 15:42:14 +0200

    Update extension.ts
    added some basic formatting.

--- src/extension.ts
+++ src/extension.ts
@@ -20,16 +20,73 @@ export function activate(context: vscode.ExtensionContext) {
     // 👍 formatter implemented using API
     vscode.languages.registerDocumentFormattingEditProvider('pde', {
         provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-            const firstLine = document.lineAt(0);
             let edit: vscode.TextEdit[] = [];
 
-            if (firstLine.text !== '42') {
-                edit.push(vscode.TextEdit.insert(firstLine.range.start, '42\n'));
+            for (var _i = 0; _i < document.lineCount; _i++) {
+                const line = document.lineAt(_i);
+                const lineS = line.text;
+
+                for (var _k = 0; _k < lineS.length; _k++) {
+
+                    if (_k + 1 < lineS.length) {
+
+                        if (lineS[_k] === '/' && lineS[_k + 1] === '/') {
+                            break;
+                        }
+
+                        if (lineS[_k] === ' ') {
+                            if (lineS[_k+1] === ';') { //no space before ";"
+                                edit.push(vscode.TextEdit.delete(new vscode.Range(line.range.start.translate(0,_k), line.range.start.translate(0,_k+1))));
+                            } else if (lineS[_k+1] === ')') { //no space before ")"
+                                edit.push(vscode.TextEdit.delete(new vscode.Range(line.range.start.translate(0,_k), line.range.start.translate(0,_k+1))));
+                            }
+                        } else if (lineS[_k] === ')') {
+                            if (lineS[_k+1] === '{') { //no space before ";"
+                                edit.push(vscode.TextEdit.insert(line.range.start.translate(0,_k+1), ' '));
+                            }
+                        }
+
+
+
+                    }
+                    if (_k + 2 < lineS.length) { 
+
+
+
+
+                        if (lineS[_k] !== ' ') {
+                            if (lineS[_k] === 'i' && lineS[_k+1] === 'f' && lineS[_k+2] === '(') { //space betwin "if" and "("
+                                edit.push(vscode.TextEdit.insert(line.range.start.translate(0,_k+2), ' '));
+                            }
+
+                            if (lineS[_k] !== '-' && lineS[_k+1] !== '-' && lineS[_k] !== '+' && lineS[_k+1] !== '+') { //dont add spaces on ++ and --
+                                if (isOpperator(lineS[_k])) { // spaces before and after opperators. example if (i==0) -> if (i == 0)
+                                    if (lineS.length > 0 && lineS[_k-1] !== ' ' && !isOpperator(lineS[_k-1])){
+                                        edit.push(vscode.TextEdit.insert(line.range.start.translate(0,_k), ' '));
+                                    }
+        
+                                    if (lineS[_k+1] !== ';' && lineS[_k+1] !== ' ' && !isOpperator(lineS[_k+1])) {
+                                        edit.push(vscode.TextEdit.insert(line.range.start.translate(0,_k+1), ' '));
+                                    }
+                                }
+                            }
+
+                        }
+
+
+                    }
+                }
             }
+
             
 			return edit;
         }
     });
+}
+
+function isOpperator(s: string) : boolean {
+    return (s === '=' || s === '<' || s === '>' || s === '&' || s === '!' || s === '|' || s === '%' 
+    || s === '+' || s === '-' || s === '*' || s === '/');
 }
 
 // this method is called when your extension is deactivated
